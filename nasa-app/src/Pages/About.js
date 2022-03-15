c3bb471e707f46477968e8ffc5339aa8321b01ec
Monika Szucs <monika.silvia.s@hotmail.ca>
2022-03-15 16:48:42 -0700

    Finished adding all content layouts to the webapp
    Now I just have to stylize the webapp.

--- nasa-app/src/Pages/About.js
+++ nasa-app/src/Pages/About.js
@@ -5,7 +5,17 @@ export default function About() {
   return (
     <div>
         <Navbar/>
-        About 
+        <h1>About</h1>
+        <h2>Creator</h2>
+        <p>Monika Szucs</p>
+        <p>Developer and Designer</p>
+        <p>Tools used: Heroku, GitHub, HTML, CSS, React, Express, Node.js, NASA API</p>
+        <p>Photoshop and Illustrator</p>
+        <h2>Links</h2>
+        <div><a href="">GitHub</a></div>
+        <div><a href="">LinkedIn</a></div>
+        <div><a href="">Behance</a></div>
+        <div><a href="">Portfolio</a></div>
     </div>
   );
 }
